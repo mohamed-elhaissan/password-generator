204dbda973681c7f6139347abe1a61531fa875fe
mohamed <mhalwasoffice@gmail.com>
2024-06-20 11:46:53 +0100

    finally

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-// // array methode
-let arr = [1, 2, 34, 4, 5, 5];
 
+const lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+const allwords = lowercaseAlphabet+uppercaseAlphabet+numbers+symbols;
+const generate = document.querySelector('button');
+const input = document.querySelector('input');
+const icon = document.getElementById('icon');
+let mylenght = 12;
+
+
+generate.addEventListener('click',function(){
+    let password = '';
+    while(password.length<mylenght){
+        password += allwords[Math.floor(Math.random()*allwords.length)];
+    }
+    icon.setAttribute('name','copy-outline');
+    input.value = password;
+})
+
+function copypassword(){
+    input.select();
+    document.execCommand('copy');
+    if (document.execCommand('copy')) {
+        icon.setAttribute('name',"checkmark-done-outline");
+        icon.style.fontSize = '25px';
+        icon.style.color = 'green';
+    }
+    else {
+        throw new Error('something wrong !');
+    }
+}
